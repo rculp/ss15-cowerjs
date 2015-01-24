db5cd82a5c2b0270edf4779314b842cc26abb53a
John Phillip Betley <jpbetley@gmail.com>
2015-01-24 09:57:16 -0500

    partialized the views

---
+++ app/js/controllers/header.js
@@ -0,0 +1,18 @@
+
+/*================================================================
+=>                  Controller = Headerctrl
+==================================================================*/
+/*global app*/
+
+app.controller('HeaderCtrl', ['$scope', function ($scope) {
+
+	'use strict';
+
+	console.log('Controller ===  HeaderCtrl');
+}]);
+
+
+/*-----  End of Controller = Headerctrl  ------*/
+
+
+
--- build/js/all.js
+++ build/js/all.js
@@ -42,3 +42,21 @@ app.run(['$rootScope', function ($rootScope) {
 /* ---> Do not delete this comment (Values) <--- */
 
 /* ---> Do not delete this comment (Constants) <--- */
+
+/*================================================================
+=>                  Controller = Headerctrl
+==================================================================*/
+/*global app*/
+
+app.controller('HeaderCtrl', ['$scope', function ($scope) {
+
+	'use strict';
+
+	console.log('Controller ===  HeaderCtrl');
+}]);
+
+
+/*-----  End of Controller = Headerctrl  ------*/
+
+
+
