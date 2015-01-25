f1a9f753ce40675a0d0fd4eb9e376ac1e38a2844
John Phillip Betley <jpbetley@gmail.com>
2015-01-25 13:32:45 -0500

    event service cleanup

--- app/js/controllers/events.js
+++ app/js/controllers/events.js
@@ -1,24 +1,30 @@
 app.controller('EventsCtrl', ['$scope', 'UserService', 'Facebook', 'EventsService', function ($scope, UserService, Facebook, EventsService) {
 	'use strict';
 
-	var _user = UserService.getCurrentUser();
-
-	$scope.username = _user.displayName;
-
 	$scope.selectedEvent = null;
-
 	$scope.loading = true;
 
 	EventsService.getEvents().then(function(events) {
 		if (events.length > 0) {
-			$scope.selectedEvent = events[0];
+			var event = events[0];
+			$scope.chooseEvent(event);
 		}
-		$scope.loading = false;
 		$scope.events = events;
 	});
 
 	$scope.chooseEvent = function(event) {
-		$scope.selectedEvent = event;
+		if (!event.dataLoaded) {
+			$scope.loading = true;
+			EventsService.getEvent(event.id).then(function(result) {
+				event.data = result.data;
+				event.facebook = result.facebook;
+				event.dataLoaded = true;
+				$scope.selectedEvent = event;
+				$scope.loading = false;
+			});
+		} else {
+			$scope.selectedEvent = event;
+		}
 	};
 
 }]);
